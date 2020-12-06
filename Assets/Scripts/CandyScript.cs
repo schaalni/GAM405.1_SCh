68799aaf007bbdfe6a8ead575cc7b01a85cb8d31
Sehrish <schaalni@gmail.com>
2020-12-06 22:10:05 +0800

    Candy Catcher

---
+++ Assets/Scripts/CandyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+        //candy should collide with the player to get detroyed
+    { if(collider.gameObject.tag == "Player")
+        {
+            //Increment Score
+            Destroy(gameObject);
+                }
+    //when player misses the candy it should get destroyed
+    else if(collider.gameObject.tag == "Boundary")
+        {
+            //Decrease lives when a cangy is missed
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool canMove = true;
+    //to change in inspector
+    [SerializeField]
+    float maxPos;
+    //to change in inspector
+    [SerializeField]
+    float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            Move();
+        }
+    }
+    private void Move()
+    {
+        //movement of player with in a given time
+        float inputX = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * inputX * moveSpeed * Time.deltaTime;
+        //player should not move beyond out of the screen
+        float xPos = Mathf.Clamp(transform.position.x, -maxPos, maxPos);
+        transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
+    }
+}
